6975b4cd9428fecb44bbc4f618bacc759223479d
rhkk7157 <rhkk7157@gmail.com>
2024-12-06 16:46:31 -0800

    feat: add get API from recipe table

--- backend/recipe/get.php
+++ backend/recipe/get.php
@@ -1,17 +1,29 @@
-<!-- 
-parameters:
-- id
+<?php
+$servername="localhost";
+$username="root";
+$password="mysql";
+$dbname = "recipe_db";
 
-return:
-- name
-- description
-- image
-- ingredients
-- steps
-- user.email
-- created_at
-- ingredients
-- likes count
-- comments
 
--->
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, name, description, created_at FROM recipe";  
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"] . "<br>";
+        echo "Recipe Name: " . $row["name"] . "<br>";
+        echo "Description: " . $row["description"] . "<br>";
+        echo "Created At: " . $row["created_at"] . "<br>";
+        echo "<hr>"; 
+    }
+} else {
+    echo "No results found";
+}
+
+$conn->close();
+?>
