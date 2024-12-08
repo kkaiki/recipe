50622b9ed376ba5e8738da25ed52c8f82915e079
Yunzoo01 <joyunzoooo@gmail.com>
2024-12-08 14:00:52 -0800

    categories-delete

--- backend/categories/delete.php
+++ backend/categories/delete.php
@@ -1,1 +1,28 @@
 <!-- to delete category -->
+ <?php
+ require '../connect.php';
+ header('Content-Type: application/json');
+
+ try{
+    $input = json_decode(file_get_contents('php://input'), true);
+    $id = $input['id'] ?? null;
+
+    if($id){
+        $db = new Database();
+        $connection = $db->getConnection();
+
+        $query = "DELETE FROM categories WHERE id = :id";
+        $stmt = $connection -> prepare($query);
+        $stmt -> bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt -> execute();
+
+        echo json_encode(['message' => 'Category deleted successfully.']);
+    } else {
+        http_response_code(400);
+        echo json_encode(['message' => 'ID is required.']);
+    }
+} catch (Exception $e) {
+    http_response_code(500);
+    echo json_encode(['message' => 'Error deleting category.', 'error' => $e->getMessage()]);
+}
+ ?>
