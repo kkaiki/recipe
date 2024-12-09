bc90deb7d575d8ca4f2411a43a15ed78b6e7daf6
kaiki <kanokaiki@gmail.com>
2024-12-08 19:45:04 -0800

    fix: add space

--- backend/users/delete.php
+++ backend/users/delete.php
@@ -17,16 +17,11 @@ try {
 
     if ($result === true) {
         echo json_encode(['message' => 'User deleted successfully.']);
-    } else {
-        $audit = new Audit($connection);
-        $audit->record($input['local_storage_user_id'], 'DELETE', "Error in delete.php", $_SERVER['REMOTE_ADDR']);
-        http_response_code(500);
-        echo json_encode(['message' => $messsage, 'error' => $result]);
     }
+
 } catch (Exception $e) {
     $audit = new Audit($connection);
     $audit->record($input['local_storage_user_id'] ?? null, 'DELETE', $e->getMessage(), $_SERVER['REMOTE_ADDR']);
-    error_log("Error in delete.php: " . $e->getMessage());
     http_response_code(500);
     echo json_encode(['message' => 'Internal server error.', 'error' => $e->getMessage()]);
 }
