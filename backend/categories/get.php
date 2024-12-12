2466ffe61de2bc9fbe2e6015231c224ef20333f2
Yunzoo01 <joyunzoooo@gmail.com>
2024-12-12 06:37:34 -0800

    changed code api

--- backend/categories/get.php
+++ backend/categories/get.php
@@ -1,8 +1,17 @@
 <!-- to get category -->
 <?php
+ header('Content-Type: application/json');
+ header('Access-Control-Allow-Origin: http://localhost:3000');
+ header('Access-Control-Allow-Methods: GET, OPTIONS');
+ header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+ }
+
  require '../connect.php';
  require_once '../auditrecord.php';
- header('Content-Type: application/json');
 
  try{
     $db = new Database();
